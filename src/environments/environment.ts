ac098514f8f9951c3b14cf10f5595517a4ce4643
artsiom <xeontem@gmail.com>
2020-07-11 12:56:06 +0300

    feat: manual deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,12 @@
+import { firebaseConfig } from './firebase-config';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCXTNRAiBMNohpqDT6xTkIP0o2mQOJRCMk",
-    authDomain: "chat-6edc2.firebaseapp.com",
-    databaseURL: "https://chat-6edc2.firebaseio.com",
-    projectId: "chat-6edc2",
-    storageBucket: "chat-6edc2.appspot.com",
-    messagingSenderId: "481500014336",
-    appId: "1:481500014336:web:9311dfd4c739bee3c975e5"
-  }
+  firebase: firebaseConfig,
 };
 
 /*
---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXTNRAiBMNohpqDT6xTkIP0o2mQOJRCMk",
+  authDomain: "chat-6edc2.firebaseapp.com",
+  databaseURL: "https://chat-6edc2.firebaseio.com",
+  projectId: "chat-6edc2",
+  storageBucket: "chat-6edc2.appspot.com",
+  messagingSenderId: "481500014336",
+  appId: "1:481500014336:web:9311dfd4c739bee3c975e5"
+};
